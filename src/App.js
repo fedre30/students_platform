ed52de9288dcbc097db9225d45ac0210d758250a
fedre30 <alfanofederica95@gmail.com>
2020-04-22 15:07:55 +0200

    Intro sectio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Navbar } from "./components/molecules/navbar";
+import { IntroSection } from "./components/organisms/intro-section";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <IntroSection />
     </div>
   );
 }
