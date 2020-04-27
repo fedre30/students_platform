f2b3373ccb61bb2bcee2742e87c8932b5eb9662e
fedre30 <alfanofederica95@gmail.com>
2020-04-27 18:20:20 +0200

    Fix

--- src/views/home.js
+++ src/views/home.js
@@ -5,6 +5,7 @@ import { AboutSection } from "../components/organisms/about-section";
 import { WorksSections } from "../components/organisms/works-section";
 import { ContactSection } from "../components/organisms/contact-section";
 import Konami from "react-konami-code";
+import { Title } from "../components/atoms/title";
 
 export const Home = () => {
   const [member, setMember] = useState(1);
@@ -18,9 +19,7 @@ export const Home = () => {
       <Navbar />
       <IntroSection />
       <Konami action={konami}>
-        {
-          "Groupe 5: Quentin Aimé, Federica Alfano, Aymeline Chemin, Mélanie Da Costa, Rainald Durand, Mathieu Gelbon, Ketsia Pedro, Marie Te"
-        }
+        <Title content="Groupe 5: Quentin Aimé, Federica Alfano, Aymeline Chemin, Mélanie Da Costa, Rainald Durand, Mathieu Gelbon, Ketsia Pedro, Marie Te" />
       </Konami>
       <AboutSection member={member} setMember={setMember} />
       <WorksSections member={member} setMember={setMember} />
